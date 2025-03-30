a5bf13624804d6dc00ea43c2b01dc05796d851be
reign <pantoniareign@gmail.com>
2025-03-30 23:45:20 +0800

    Fixed routing for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter  as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Player from "./components/Player";
 import Share from "./components/Share";
